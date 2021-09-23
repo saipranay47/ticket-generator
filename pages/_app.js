e63d1be739963efa0bddbb5d5cd3ce9bc46673aa
Sai Pranay <saipranayadepu07@gmail.com>
2021-09-23 11:26:19 +0530

    small changes

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
 import '../styles/globals.css'
+import {Provider as AuthProvider} from "next-auth/client"
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+const MyApp = ({ Component, pageProps }) => {
+  return (
+    <AuthProvider>
+      <Component {...pageProps} />
+    </AuthProvider>
+  )
 }
 
 export default MyApp
